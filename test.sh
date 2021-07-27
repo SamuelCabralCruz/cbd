8dfe63ad5688285518ff6813aa6f4fc3a1144297
Samuel Cabral Cruz <samuelcabralcruz@gmail.com>
2021-07-27 13:26:20 -0400

    first working version

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+BRANCH_NAME="test-open"
+git stash save
+git checkout main
+git branch -D $BRANCH_NAME
+git stash apply
+git add -A && git commit --no-edit --amend && git push -f
+git checkout -b $BRANCH_NAME
+touch $BRANCH_NAME.txt
+git add -A
+git commit -m "$BRANCH_NAME"
+git push origin -uf $BRANCH_NAME
+git checkout main
