3228049e3ed26812ccee7f7bf8a380ac6c7ab9cb
Samuel Cabral Cruz <samuelcabralcruz@gmail.com>
2021-07-29 20:25:01 -0400

    more resilient pull request commenting

--- shared/github.sh
+++ shared/github.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+is_already_commented() {
+  local GITHUB_TOKEN=$1
+  local COMMENTS_URL=$2
+  local COMMENT_CONTENT=$3
+  curl --request GET \
+    --url "$COMMENTS_URL" \
+    --header "authorization: Bearer $GITHUB_TOKEN" \
+    --header 'content-type: application/json' \
+    --header 'accept: application/vnd.github.v3+json' \
+    -o "pull_request_comments.json" &> /dev/null
+  if [[ $(jq ".[] | select(.body | match(\"$COMMENT_CONTENT\")) | any" pull_request_comments.json) == 'true' ]]; then
+    echo 'true';
+  else
+    echo 'false';
+  fi
+}
+
 create_pull_request_comment() {
   local GITHUB_TOKEN=$1
   local CREATE_COMMENT_URL=$2
